7890a53d9ab712d4fffc949200445e0f3dea11de
Carlton Ncube <carltonncube@outlook.com>
2024-04-02 21:46:52 +0200

    a class FIFOCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """
+    Inherits from BaseCaching and is a caching system using FIFO algorithm
+    """
+
+    def __init__(self):
+        """
+        Initialize the FIFO cache
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                # If cache is full, remove the first item (FIFO)
+                discarded_key = self.queue.pop(0)
+                del self.cache_data[discarded_key]
+                print("DISCARD:", discarded_key)
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
