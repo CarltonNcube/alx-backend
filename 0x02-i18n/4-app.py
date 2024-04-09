8be36286103c474a43bcd847d5fb461ca6ec1707
Carlton Ncube <carltonncube@outlook.com>
2024-04-10 00:16:54 +0200

    a user login system

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -7,26 +7,44 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
-# Configuration
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
-app.config['LANGUAGES'] = ['en', 'fr']  # Supported languages
+
+class Config:
+    """
+    Configuration class for the Flask app
+    """
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
 
 @babel.localeselector
-def get_locale():
-    # Check if the 'locale' parameter is present in the request URL
-    locale_param = request.args.get('locale')
-    if locale_param and locale_param in app.config['LANGUAGES']:
-        return locale_param
-    # Resort to the default behavior
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_locale() -> str:
+    """
+    Determine the best-matching language from the request
+    """
+    # Check if the request contains the 'locale' argument
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index() -> str:
+    """
+    Renders index.html template
+    """
+    return render_template('4-index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
 
