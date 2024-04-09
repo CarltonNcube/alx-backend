6e1c596d085723145244b20eb5287e1049e32256
Carlton Ncube <carltonncube@outlook.com>
2024-04-09 22:24:41 +0200

    get_locale function that detect if the incoming request contains locale argument and its value

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
@@ -14,26 +15,30 @@ class Config:
     """
     Configuration class for the Flask app
     """
+    DEBUG = True
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app.config.from_object(Config)
+
+
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """
     Determine the best-matching language from the request
     """
     # Check if the request contains the 'locale' argument
     locale = request.args.get('locale')
-    if locale in Config.LANGUAGES:
+    if locale in app.config['LANGUAGES']:
         return locale
     else:
-        return request.accept_languages.best_match(Config.LANGUAGES)
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """
     Renders index.html template
     """
@@ -42,3 +47,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
+
