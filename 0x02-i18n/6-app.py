e61e66424041cf2dc14529cf33c4b64b24b80edc
Carlton Ncube <carltonncube@outlook.com>
2024-04-10 19:27:48 +0200

    get_locale function, detect if the incoming request contains locale argument

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -17,6 +17,7 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 def get_user():
     """
     Retrieve user based on login_as URL parameter
@@ -24,12 +25,14 @@ def get_user():
     user_id = int(request.args.get('login_as', 0))
     return users.get(user_id)
 
+
 @app.before_request
 def before_request():
     """
     Executed before all other functions
     """
     g.user = get_user()
+
 
 @babel.localeselector
 def get_locale():
@@ -43,12 +46,14 @@ def get_locale():
         return g.user['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """
     Renders index.html template
     """
     return render_template('6-index.html')
+
 
 if __name__ == '__main__':
     app.run()
