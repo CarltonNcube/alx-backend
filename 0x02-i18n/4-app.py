6d7c7a7613d6b3ad7ffec1b000b667292337bffb
Carlton Ncube <carltonncube@outlook.com>
2024-04-10 21:26:32 +0200

    get_locale function, detect if the incoming request contains locale argument

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -6,45 +6,49 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-babel = Babel(app)
-
 
 class Config:
-    """
-    Configuration class for the Flask app
-    """
+    '''Config class'''
+
     DEBUG = True
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
     """
-    Determine the best-matching language from the request
-    """
-    # Check if the request contains the 'locale' argument
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
-    """
-    Renders index.html template
-    """
-    return render_template('4-index.html')
+    '''default route
+
+    Returns:
+        html: homepage
+    '''
+    return render_template("4-index.html")
+
+# uncomment this line and comment the @babel.localeselector
+# you get this error:
+# AttributeError: 'Babel' object has no attribute 'localeselector'
+# babel.init_app(app, locale_selector=get_locale)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
-
+if __name__ == "__main__":
+    app.run()
