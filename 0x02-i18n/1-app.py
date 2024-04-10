57a5a83ecab4dcc7c689f216f0e0bb01a5d71eb3
Carlton Ncube <carltonncube@outlook.com>
2024-04-10 18:13:16 +0200

    a Config class that has a LANGUAGES class attribute equal to [en, fr].

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,10 +6,7 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
-
 app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config:
     """
@@ -19,6 +16,9 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+app.config.from_object(Config)
+
+babel = Babel(app)
 
 @app.route('/')
 def index():
@@ -26,7 +26,6 @@ def index():
     Renders index.html template
     """
     return render_template('1-index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
